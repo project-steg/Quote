ad9fd8fe6513ac6209bff25c9f8c554cbca4f8b4
kspace-trk <keigonakao.0825@gmail.com>
2020-09-13 19:30:46 +0900

    [add]btn

---
+++ components/how2use.vue
@@ -0,0 +1,458 @@
+<template>
+  <div class="how2use-wrapper">
+    <div class="how2use-label">
+      <p>使い方</p>
+    </div>
+    <div class="how2use-container">
+      <!---------------------------------------------------------->
+      <div class="how2use-contents">
+        <div class="how2use-contents-l">
+          <div class="empty-stick"></div>
+          <div class="circle">
+            <v-icon m-large dark>mdi-align-horizontal-left</v-icon>
+          </div>
+          <div class="stick"></div>
+        </div>
+        <div class="how2use-contents-c">
+          <div class="how2use-step">Step1</div>
+          <div class="how2use-title">見積もり</div>
+          <div class="how2use-subtitle">Quote</div>
+        </div>
+        <div class="how2use-contents-r">
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="empty-stick-min"></div>
+              <div class="circle-min">1</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">制作するWebサイトに載せたいコンテンツを選択する</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">2</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">見積もりボタンを押して、見積もり金額を確認する</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">3</div>
+              <div class="empty-stick-min"></div>
+            </div>
+            <div class="how2use-details-r">「この内容でお問い合わせる」ボタンを押してお問い合わせを送る</div>
+          </div>
+        </div>
+      </div>
+      <!---------------------------------------------------------->
+      <div class="how2use-contents">
+        <div class="how2use-contents-l">
+          <div class="stick"></div>
+          <div class="circle">
+            <svg
+              id="Layer_1"
+              enable-background="new 0 0 512 512"
+              height="512"
+              viewBox="0 0 512 512"
+              width="512"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path
+                  d="m205.182 143.679h95.259c8.284 0 15-6.716 15-15s-6.716-15-15-15h-95.259c-8.284 0-15 6.716-15 15s6.715 15 15 15z"
+                />
+                <path
+                  d="m366.03 0h-351.03c-8.284 0-15 6.716-15 15v482c0 8.284 6.716 15 15 15h351.03c8.284 0 15-6.716 15-15v-482c0-8.284-6.716-15-15-15zm-15 482h-321.03v-452h321.03z"
+                />
+                <path
+                  d="m512 363.502v-312.591c0-28.072-22.839-50.911-50.911-50.911-28.073 0-50.912 22.839-50.912 50.911v312.59c0 7.826 5.996 14.245 13.643 14.931l-12.748 35.22c-1.34 3.703-1.171 7.784.472 11.362l35.912 78.242c2.445 5.328 7.771 8.743 13.633 8.743s11.188-3.415 13.633-8.743l35.911-78.242c1.643-3.579 1.812-7.66.472-11.362l-12.748-35.22c7.647-.686 13.643-7.104 13.643-14.93zm-71.823-15v-223.596h41.823v223.595h-41.823zm20.912-318.502c11.53 0 20.911 9.381 20.911 20.911v43.995h-41.823v-43.995c0-11.53 9.381-20.911 20.912-20.911zm0 431.041-19.716-42.956 14.327-39.584h10.777l14.328 39.584z"
+                />
+                <path
+                  d="m90.701 157.197c2.813 2.813 6.628 4.394 10.606 4.394s7.794-1.581 10.606-4.393l45.563-45.563c5.858-5.858 5.858-15.355 0-21.213-5.857-5.858-15.355-5.858-21.213 0l-34.956 34.956-10.866-10.868c-5.856-5.858-15.354-5.858-21.213 0-5.858 5.858-5.858 15.355 0 21.213z"
+                />
+                <path
+                  d="m205.182 234.684h95.259c8.284 0 15-6.716 15-15s-6.716-15-15-15h-95.259c-8.284 0-15 6.716-15 15s6.715 15 15 15z"
+                />
+                <path
+                  d="m90.701 248.201c2.813 2.813 6.628 4.394 10.606 4.394s7.794-1.581 10.606-4.393l45.563-45.563c5.858-5.858 5.858-15.355 0-21.213-5.857-5.858-15.355-5.858-21.213 0l-34.956 34.956-10.866-10.866c-5.856-5.858-15.354-5.858-21.213 0-5.858 5.858-5.858 15.355 0 21.213z"
+                />
+                <path
+                  d="m205.182 325.688h95.259c8.284 0 15-6.716 15-15s-6.716-15-15-15h-95.259c-8.284 0-15 6.716-15 15s6.715 15 15 15z"
+                />
+                <path
+                  d="m205.182 420.178h95.259c8.284 0 15-6.716 15-15s-6.716-15-15-15h-95.259c-8.284 0-15 6.716-15 15s6.715 15 15 15z"
+                />
+                <path
+                  d="m90.701 339.206c2.813 2.813 6.628 4.394 10.606 4.394s7.794-1.581 10.606-4.393l45.563-45.562c5.858-5.858 5.858-15.355 0-21.213-5.857-5.858-15.355-5.857-21.213 0l-34.956 34.956-10.866-10.868c-5.856-5.858-15.354-5.858-21.213 0s-5.858 15.355 0 21.213z"
+                />
+              </g>
+            </svg>
+          </div>
+          <div class="stick"></div>
+        </div>
+        <div class="how2use-contents-c">
+          <div class="how2use-step">Step2</div>
+          <div class="how2use-title">制作依頼</div>
+          <div class="how2use-subtitle">Request</div>
+        </div>
+        <div class="how2use-contents-r">
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="empty-stick-min"></div>
+              <div class="circle-min">1</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">送信したお問い合わせに対しての返信を確認する</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">2</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">詳細な見積もり、制作物の確認のやりとりを行う</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">3</div>
+              <div class="empty-stick-min"></div>
+            </div>
+            <div class="how2use-details-r">予算・制作物ともに合意したら、Webサイトの制作依頼が完了する</div>
+          </div>
+        </div>
+      </div>
+      <!---------------------------------------------------------->
+      <div class="how2use-contents">
+        <div class="how2use-contents-l">
+          <div class="stick"></div>
+          <div class="circle">
+            <svg
+              height="424pt"
+              viewBox="0 -25 424 424"
+              width="424pt"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="m167.289062 272.132812c-1.601562 0-3.214843-.550781-4.53125-1.671874l-45.296874-38.570313c-1.5625-1.332031-2.460938-3.277344-2.460938-5.332031 0-2.050782.902344-3.996094 2.460938-5.328125l45.296874-38.570313c2.941407-2.507812 7.359376-2.152344 9.867188.789063 2.503906 2.945312 2.152344 7.363281-.792969 9.867187l-39.035156 33.242188 39.035156 33.242187c2.945313 2.507813 3.296875 6.925781.792969 9.867188-1.386719 1.628906-3.355469 2.464843-5.335938 2.464843zm0 0"
+              />
+              <path
+                d="m256.710938 272.132812c-1.980469 0-3.949219-.835937-5.332032-2.464843-2.507812-2.941407-2.152344-7.359375.789063-9.867188l39.035156-33.242187-39.035156-33.242188c-2.941407-2.503906-3.296875-6.921875-.789063-9.867187 2.503906-2.941407 6.925782-3.296875 9.863282-.789063l45.296874 38.570313c1.5625 1.332031 2.460938 3.277343 2.460938 5.328125 0 2.054687-.898438 4-2.460938 5.332031l-45.296874 38.570313c-1.316407 1.121093-2.929688 1.671874-4.53125 1.671874zm0 0"
+              />
+              <path
+                d="m195.964844 301.0625c-.480469 0-.964844-.050781-1.453125-.152344-3.78125-.796875-6.203125-4.511718-5.40625-8.292968l28.230469-134.035157c.796874-3.78125 4.507812-6.203125 8.292968-5.40625 3.78125.796875 6.203125 4.507813 5.40625 8.292969l-28.230468 134.035156c-.695313 3.296875-3.601563 5.558594-6.839844 5.558594zm0 0"
+              />
+              <path
+                d="m377 374.085938h-330c-25.914062 0-47-21.082032-47-47v-280.085938c0-25.914062 21.085938-47 47-47h330c25.914062 0 47 21.085938 47 47v280.085938c0 25.917968-21.085938 47-47 47zm-330-360.085938c-18.195312 0-33 14.804688-33 33v280.085938c0 18.195312 14.804688 33 33 33h330c18.195312 0 33-14.804688 33-33v-280.085938c0-18.195312-14.804688-33-33-33zm0 0"
+              />
+              <path
+                d="m417 112.089844h-410c-3.867188 0-7-3.132813-7-7 0-3.863282 3.132812-7 7-7h410c3.867188 0 7 3.136718 7 7 0 3.867187-3.132812 7-7 7zm0 0"
+              />
+              <path
+                d="m119.601562 78.59375c-12.210937 0-22.152343-9.941406-22.152343-22.152344 0-12.214844 9.941406-22.152344 22.152343-22.152344 12.214844 0 22.152344 9.9375 22.152344 22.152344 0 12.210938-9.9375 22.152344-22.152344 22.152344zm0-30.304688c-4.492187 0-8.152343 3.65625-8.152343 8.152344s3.660156 8.152344 8.152343 8.152344c4.496094 0 8.152344-3.65625 8.152344-8.152344s-3.65625-8.152344-8.152344-8.152344zm0 0"
+              />
+              <path
+                d="m51.539062 78.378906c-12.214843 0-22.152343-9.9375-22.152343-22.152344 0-12.214843 9.9375-22.152343 22.152343-22.152343 12.214844 0 22.152344 9.9375 22.152344 22.152343 0 12.214844-9.9375 22.152344-22.152344 22.152344zm0-30.304687c-4.492187 0-8.152343 3.660156-8.152343 8.152343 0 4.496094 3.660156 8.152344 8.152343 8.152344 4.496094 0 8.152344-3.65625 8.152344-8.152344 0-4.492187-3.65625-8.152343-8.152344-8.152343zm0 0"
+              />
+              <path
+                d="m187.664062 78.804688c-12.210937 0-22.148437-9.9375-22.148437-22.152344 0-12.210938 9.9375-22.148438 22.148437-22.148438 12.214844 0 22.152344 9.9375 22.152344 22.148438 0 12.214844-9.9375 22.152344-22.152344 22.152344zm0-30.304688c-4.492187 0-8.148437 3.65625-8.148437 8.152344s3.65625 8.152344 8.148437 8.152344c4.496094 0 8.152344-3.65625 8.152344-8.152344s-3.65625-8.152344-8.152344-8.152344zm0 0"
+              />
+            </svg>
+          </div>
+          <div class="stick"></div>
+        </div>
+        <div class="how2use-contents-c">
+          <div class="how2use-step">Step3</div>
+          <div class="how2use-title">開発</div>
+          <div class="how2use-subtitle">Develop</div>
+        </div>
+        <div class="how2use-contents-r">
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="empty-stick-min"></div>
+              <div class="circle-min">1</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">当デザイナー・エンジニアが開発を担当する</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">2</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">コンテンツ内の修正や変更は基本的に無料で行える</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">3</div>
+              <div class="empty-stick-min"></div>
+            </div>
+            <div class="how2use-details-r">完成した制作物を確認してもらう</div>
+          </div>
+        </div>
+      </div>
+      <!---------------------------------------------------------->
+      <div class="how2use-contents">
+        <div class="how2use-contents-l">
+          <div class="stick"></div>
+          <div class="circle">
+            <svg
+              id="Capa_1"
+              enable-background="new 0 0 511.99 511.99"
+              height="512"
+              viewBox="0 0 511.99 511.99"
+              width="512"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path
+                  d="m446.822 213.631-190.827-63.256-190.827 63.256-65.158 103.498 59.984 19.884v110.007l196 64.97 196-64.97v-110.007l59.984-19.884zm-57.5 12.545-133.327 44.195-133.327-44.195 133.327-44.195zm-343.52 74.527 35.627-56.592 151.67 50.276-35.626 56.591zm44.193 46.255 120.345 39.892 30.655-48.693v137.256l-151-50.054zm181 128.454v-137.256l30.655 48.693 120.345-39.892v78.401zm43.522-124.433-35.626-56.591 151.67-50.276 35.627 56.592z"
+                />
+                <path d="m240.995 0h30v100.174h-30z" />
+                <path
+                  d="m122.952 31.63h30v100.174h-30z"
+                  transform="matrix(.866 -.5 .5 .866 -22.376 79.924)"
+                />
+                <path
+                  d="m323.951 66.717h100.174v30h-100.174z"
+                  transform="matrix(.5 -.866 .866 .5 116.249 364.784)"
+                />
+              </g>
+            </svg>
+          </div>
+          <div class="empty-stick"></div>
+        </div>
+        <div class="how2use-contents-c">
+          <div class="how2use-step">Step4</div>
+          <div class="how2use-title">納品</div>
+          <div class="how2use-subtitle">Delivery</div>
+        </div>
+        <div class="how2use-contents-r">
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="empty-stick-min"></div>
+              <div class="circle-min">1</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">完成した制作物に不備・不満がないか確認する</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">2</div>
+              <div class="stick-min"></div>
+            </div>
+            <div class="how2use-details-r">制作料金を支払う</div>
+          </div>
+          <div class="how2use-details-wrapper">
+            <div class="how2use-details-l">
+              <div class="stick-min"></div>
+              <div class="circle-min">3</div>
+              <div class="empty-stick-min"></div>
+            </div>
+            <div class="how2use-details-r">納品完了</div>
+          </div>
+        </div>
+      </div>
+      <!---------------------------------------------------------->
+    </div>
+    <div class="how2use-btn">
+      <button>
+        <p>見積もってみる</p>
+      </button>
+    </div>
+  </div>
+</template>
+<style scoped>
+.how2use-wrapper {
+  width: 100%;
+  margin: 60px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.how2use-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.how2use-label {
+  width: 60%;
+  height: 80px;
+  background: #20d8ba;
+  padding: 0.4em 0.8em 0.3em;
+  margin: 0 0 1em;
+  text-align: left;
+  color: #000;
+  font-size: 24px;
+  font-weight: bold;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.how2use-label::after {
+  position: absolute;
+  content: "";
+  top: 100%;
+  left: 30px;
+  border: 15px solid transparent;
+  border-top: 15px solid #20d8ba;
+  width: 0;
+  height: 0;
+}
+.how2use-label p {
+  color: #ffffff;
+  font-size: 1.2rem;
+  margin-bottom: 5px;
+}
+.how2use-contents {
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  border-bottom: 2px dashed #c9f4ed;
+}
+.how2use-contents-l {
+  width: 20%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.how2use-contents-c {
+  width: 20%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 25px;
+}
+.how2use-contents-r {
+  width: 60%;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+@media screen and (max-width: 480px) {
+  .how2use-contents-l {
+    display: none;
+  }
+  .how2use-contents-c {
+    width: 40%;
+    padding-left: 0;
+  }
+}
+.empty-stick {
+  width: 0;
+  height: 100px;
+}
+.circle {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #20d8ba;
+  display: grid;
+  place-items: center;
+}
+.circle svg {
+  width: 30px;
+  height: 30px;
+  fill: #ffffff;
+}
+.stick {
+  width: 7px;
+  height: 100px;
+  background-color: #20d8ba;
+}
+.how2use-step {
+  color: #4cdfc7;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.how2use-title {
+  color: #17171b;
+  font-size: 1.2rem;
+}
+.how2use-subtitle {
+  color: #909090;
+}
+.how2use-details-wrapper {
+  width: 100%;
+  height: 85px;
+  display: flex;
+  align-items: center;
+}
+.empty-stick-min {
+  width: 0;
+  height: 25px;
+}
+.circle-min {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  background-color: #20d8ba;
+  display: grid;
+  place-items: center;
+  font-weight: 700;
+  font-size: 0.9rem;
+  padding-bottom: 2px;
+  color: #ffffff;
+}
+.stick-min {
+  width: 3px;
+  height: 25px;
+  background-color: #20d8ba;
+}
+.how2use-details-l {
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.how2use-details-r {
+  width: 80%;
+  font-weight: 700;
+  color: #17171b;
+}
+@media screen and (max-width: 480px) {
+  .how2use-details-r {
+    padding-left: 15px;
+    font-size: 0.8rem;
+  }
+}
+.how2use-btn button {
+  width: 500px;
+  height: 75px;
+  margin-top: 50px;
+  background-color: #20d8ba;
+  display: grid;
+  place-items: center;
+}
+.how2use-btn button:hover {
+  width: 500px;
+  height: 75px;
+  margin-top: 50px;
+  background-color: #ffffff;
+  border: 4px solid #20d8ba;
+  display: grid;
+  place-items: center;
+}
+.how2use-btn p {
+  color: #ffffff;
+  font-weight: 700;
+}
+.how2use-btn button:hover p {
+  color: #20d8ba;
+  font-weight: 700;
+}
+p {
+  margin: 0;
+}
+</style>
