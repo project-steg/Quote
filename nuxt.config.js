f767a48aaf5a75b901d8e8d0b171479bbab40de8
kspace-trk <keigonakao.0825@gmail.com>
2020-10-27 04:49:17 +0900

    [add]OGP

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,22 +10,55 @@ export default {
       lang: "ja",
       prefix: "og: http://ogp.me/ns#"
     },
-    title: process.env.npm_package_name || "",
+    title: "Steg Webサイト制作料金見積もりシステム",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || ""
-      }
+        content:
+          "Steg Webサイト制作料金見積もりシステム。無料で簡単にWebサイト制作の予算見積もりができます。気に入っていただければそのまま制作依頼をすることができます。"
+      },
+      {
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "Steg Webサイト制作料金見積もりシステム"
+      },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://quote-steg.netlify.app"
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Steg Webサイト制作料金見積もりシステム"
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content:
+          "Steg Webサイト制作料金見積もりシステム。無料で簡単にWebサイト制作の予算見積もりができます。気に入っていただければそのまま制作依頼をすることができます。"
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://quote-steg.netlify.app/img/ogp.png"
+      },
+      {
+        hid: "twitter:card",
+        name: "twitter:card",
+        content: "summary_large_image"
+      },
+      { hid: "twitter:site", name: "twitter:site", content: "@Steg_official" }
     ],
     link: [
       {
         rel: "icon",
         type: "image/x-icon",
         href: "/favicon.ico",
-        rel: "stylesheet",
         href: "https://fonts.googleapis.com/css?family=Roboto:700",
         rel: "stylesheet",
         href:
